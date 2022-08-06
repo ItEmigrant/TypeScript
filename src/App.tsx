8af72d5a8473657d137129215eb834922e3f4996
Bohdan Solomchak <bohdan.jur@gmail.com>
2022-08-07 00:17:35 +0200

    commit1

--- src/App.tsx
+++ src/App.tsx
@@ -4,61 +4,62 @@ import './App.css';
 
 function App() {
 
-  return (
-      <div>
-        <AppTitle/>
-        <Rating/>
-        <Accordion/>
-        <Rating/>
-      </div>
-  );
+    return (
+        <div>
+            <AppTitle/>
+            <Rating/>
+            <Accordion/>
+            <Rating/>
+        </div>
+    );
 
 }
 
 function Rating() {
-  console.log("Rating rendering")
-  return (<div>
-    <Star/>
-    <Star/>
-    <Star/>
-    <Star/>
-  </div>);
+    console.log("Rating rendering")
+    return (<div>
+        <Star/>
+        <Star/>
+        <Star/>
+        <Star/>
+    </div>);
 
 }
 
 function Accordion() {
-  console.log("Accordion rendering")
-  return <>
-    <AccordionTitle/>
-    <AccordionBody/>
-  </>;
+    console.log("Accordion rendering")
+    return <>
+        <AccordionTitle/>
+        <AccordionBody/>
+    </>;
 }
 
 function AccordionTitle() {
-  console.log("AccordionTitle rendering")
-  return <>Menu</>
+    console.log("AccordionTitle rendering")
+    return <>Menu</>
 }
 
 function AccordionBody() {
-  console.log("AccordionBody rendering")
-  return <>
-    <ul>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
-      <li>5</li>
-      <li>6</li>
-    </ul>
-  </>
+    console.log("AccordionBody rendering")
+    return <>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>5</li>
+            <li>6</li>
+        </ul>
+    </>
 }
 
 function Star() {
-  console.log("Star rendering")
-  return <div>star</div>;
+    console.log("Star rendering")
+    return <div>star</div>;
 }
 
 function AppTitle() {
-  return <>This is APP component</>
+  return <>This is APP component!</>
 }
+
 export default App;
