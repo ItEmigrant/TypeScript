2d3e8264d23ff45b4c9946c3f7e30000b4b30768
Bohdan Solomchak <bohdan.jur@gmail.com>
2022-09-02 15:59:16 +0200

    lesson6useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
 import './App.css';
-import Accordion from './components/Accordion/Accordion';
-import {Rating} from "./components/Rating/Rating";
+/*import Accordion from './components/Accordion/Accordion';
+import {Rating} from "./components/Rating/Rating";*/
 import {OnOff} from "./components/OnOff/OnOff";
 
 function App() {
 
     return (
         <div>
-            <PageTitle title={"This is APP component!"}/>
+           {/* <PageTitle title={"This is APP component!"}/>
             <PageTitle title={"My friends!"}/>
             Article 1
             <Rating value={3}/>
@@ -20,19 +20,21 @@ function App() {
             <Rating value={1}/>
             <Rating value={2}/>
             <Rating value={4}/>
-            <Rating value={5}/>
-            <OnOff button={false}/>
+            <Rating value={5}/>*/}
+            <OnOff />
+
+
         </div>
     );
 
 }
 
-type PageTitleProps = {
+/*type PageTitleProps = {
     title: string
-}
+}*/
 
-function PageTitle(props: PageTitleProps) {
+/*function PageTitle(props: PageTitleProps) {
     return <><h1>{props.title}</h1></>
-}
+}*/
 
 export default App;
--- src/components/OnOff/OnOff.tsx
+++ src/components/OnOff/OnOff.tsx
@@ -1,18 +1,46 @@
-import React from 'react';
-type propsOnOFType ={
-    button:boolean
+import React, {useState} from 'react';
+
+
+
+export function OnOff() {
+   console.log ('')
+
+  let [on, setOn]= useState(false);
+
+
+    const onStyle = {
+        width: '30px',
+        height: '20px',
+        border: '1px solid black',
+        display: 'inline-block',
+        padding: '2px',
+        backgroundColor: on ? "green" : "white"
+    };
+    const ofStyle = {
+        width: '30px',
+        height: '20px',
+        border: '1px solid black',
+        display: 'inline-block',
+        marginLeft: "2px",
+        padding: '2px',
+        backgroundColor: on ? "white" : "red"
+    };
+    const indicatorStyle = {
+        width: '10px',
+        height: '10px',
+        borderRadius: '5px',
+        border: "1px solid black",
+        display: 'inline-block',
+        marginLeft: "5px",
+        backgroundColor: on ? "green" : "red"
+    };
+
+    return <div>
+        <div style={onStyle} onClick={() => {setOn(true)}}>On </div>
+        <div style={ofStyle} onClick={() => {setOn( false)}}>Off </div>
+        <div style={indicatorStyle}></div>
+    </div>
+
+
 }
-export const OnOff = (props:propsOnOFType) => {
-
-
-    // eslint-disable-next-line no-lone-blocks
-    {
-        if (props.button) {
-            return <div id='SQUARE'>ON</div>
-        } else {
-            return <div id='SQUARE1'>OF</div>
-        }
-    }
-
-};
 
