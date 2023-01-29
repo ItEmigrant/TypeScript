de26b08ef6322743588c55bc27bff30447a5d5d3
Bohdan Solomchak <bohdan.jur@gmail.com>
2023-01-29 12:45:57 +0100

    lesson16.1Reducer

--- src/components/UnAccordion/UnAccordion.tsx
+++ src/components/UnAccordion/UnAccordion.tsx
@@ -1,33 +1,57 @@
-import React, {useState} from "react";
+import React, {useReducer} from "react";
 
 type AccordionPropsType1 = {
     titleValue: string
     //collapsed: boolean
 }
 
-export function UnAccordion (props: AccordionPropsType1) {
+type reducerActionType = {
+    type: "TOGGLE-COLLAPSED"
+
+}
+
+
+const reducerUnAccording = (state: boolean, action: reducerActionType) => {
+
+    switch (action.type) {
+        case "TOGGLE-COLLAPSED":
+            return !state;
+
+        default:
+            return state;
+    }
+
+}
+
+export function UnAccordion(props: AccordionPropsType1) {
     console.log("Accordion rendering")
 
-    let [collapsed, setCollapsed] = useState(true);
+    /* let [collapsed, setCollapsed] = useState(true);*/
+    let [collapsed, dispatch] = useReducer(reducerUnAccording, true);
 
-        return <div>
-            <AccordionTitle title={props.titleValue} onClick={()=>(setCollapsed(!collapsed))}/>
+    return <div>
+        <AccordionTitle title={props.titleValue} onClick={() => {
+
+            dispatch({type: "TOGGLE-COLLAPSED"})
+        }}/>
 
 
-            {!collapsed && <AccordionBody/>}
-        </div>
+        {!collapsed && <AccordionBody/>}
+    </div>
 
 }
 
 
 type AccordionTitlePropsType = {
     title: string
-    onClick: ()=>void
+    onClick: () => void
 }
 
 function AccordionTitle(props: AccordionTitlePropsType) {
     console.log("AccordionTitle rendering")
-    return <><h3 onClick={()=>{props.onClick()}}>=={props.title }==</h3></>
+    return <><h3 onClick={() => {
+        props.onClick()
+    }}>=={props.title}==</h3></>
 }
 
 function AccordionBody() {
